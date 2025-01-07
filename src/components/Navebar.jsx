d74815a17a18cadce1e79bcc7c7d0a35e8e600e1
Tawhid-01 <mtworld008@gmail.com>
2025-01-07 22:07:00 +0600

    hover

--- src/components/Navebar.jsx
+++ src/components/Navebar.jsx
@@ -22,12 +22,12 @@ const Navbar = () => {
                             <img src={Logo} alt="Logo" />
                         </div>
                         <div className="">
-                            <ul className={`lg:flex lg:gap-8 lg:static lg:bg-transparent ${menuShow == true ? '  absolute top-20 left-0 bg-slate-300  duration-700 py-10 px-7 flex flex-col gap-7 t' : 'absolute -left-44 '} `}>
-                                <li><Link to="/" className="text-black  hover:underline decoration-violet-900 decoration-4 ">Home</Link></li>
-                                <li><Link to="/shop" className=":text-black  hover:underline decoration-violet-900 decoration-4">Shop</Link></li>
-                                <li className='group relative'><Link className=" flex items-center text-black  hover:underline decoration-violet-900 decoration-4">Pages<RxTriangleDown /></Link>
+                            <ul className={`lg:flex lg:gap-8 lg:static lg:bg-transparent  ${menuShow == true ? '  absolute top-20 left-0 bg-slate-300  duration-700 py-10 px-7 flex flex-col gap-7 t' : 'absolute -left-44 '} `}>
+                                <li><Link to="/" className="text-black  decoration-violet-900 decoration-4   hover:bg-purple-300 rounded-xl px-1 py-1 ">Home</Link></li>
+                                <li><Link to="/shop" className=":text-black  hover:underline decoration-violet-900 decoration-4  hover:bg-purple-300 rounded-xl px-4 py-1 ">Shop</Link></li>
+                                <li className='group relative'><Link className=" flex items-center text-black   decoration-violet-900 decoration-4  hover:bg-purple-300 rounded-xl px-1 py-1 ">Pages<RxTriangleDown /></Link>
                                   
-                                  <ul className=' absolute  lg:hidden lg:group-hover:block px-3  bg-slate-400 hidden group-hover:block'>
+                                  <ul className=' absolute  lg:hidden lg:group-hover:block px-3  hidden group-hover:block   hover:bg-purple-300 rounded-xl  bg-purple-200 py-1 '>
                                     <li className='pb-3'><Link>page1</Link></li>
                                     <li className='pb-3'><Link>page2</Link></li>
                                     <li className='pb-3'><Link>page3</Link></li>
@@ -37,9 +37,9 @@ const Navbar = () => {
 
 
                                 </li>
-                                <li><Link to="/products" className="text-black  hover:underline decoration-violet-900 decoration-4">Products</Link></li>
-                                <li><Link to="/blog" className=" text-black  hover:underline decoration-violet-900 decoration-4">Blog</Link></li>
-                                <li><Link to="/contacts" className="text-black  hover:underline decoration-violet-900 decoration-4">Contacts</Link></li>
+                                <li><Link to="/products" className="text-black  hover:bg-purple-300 rounded-xl px-1 py-1  decoration-violet-900 decoration-4">Products</Link></li>
+                                <li><Link to="/blog" className=" text-black   hover:bg-purple-300 rounded-xl px-4 py-1  decoration-violet-900 decoration-4">Blog</Link></li>
+                                <li><Link to="/contacts" className="text-black  hover:bg-purple-300 rounded-xl px-1 py-1  decoration-violet-900 decoration-4">Contacts</Link></li>
                             </ul>
                         </div>
                     </div>
