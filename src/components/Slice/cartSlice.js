13768f9117ea0d35066baf3d1fca951a68f1b0c5
Tawhid-01 <mtworld008@gmail.com>
2025-01-14 19:15:19 +0600

    new

--- src/components/Slice/cartSlice.js
+++ src/components/Slice/cartSlice.js
@@ -1,26 +1,49 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const cartSlice = createSlice({
-  name: 'cartItemManegment',
+  name: 'cartItemManagement',
   initialState: {
     cartItem: localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
   },
   reducers: {
     addToCart: (state, action) => {
-
-    let findProduct = state.cartItem.findIndex((item)=>item.id == action.payload.id)
+      const findProduct = state.cartItem.findIndex(item => item.id === action.payload.id);
                              
-    if(findProduct === -1){
-          state.cartItem.push(action.payload);
-       } else {
-         state.cartItem[findProduct].qty += 1
-       }
+      if (findProduct === -1) {
+        state.cartItem.push({ ...action.payload, qty: 1 });
+      } else {
+        state.cartItem[findProduct].qty += 1;
+      }
       
-         localStorage.setItem('cart', JSON.stringify(state.cartItem));
+      localStorage.setItem('cart', JSON.stringify(state.cartItem));
+    },
+    increment: (state, action) => {
+      const findProduct = state.cartItem.findIndex(item => item.id === action.payload);
+      
+      if (findProduct !== -1) {
+        state.cartItem[findProduct].qty += 1;
+        localStorage.setItem('cart', JSON.stringify(state.cartItem));
+      }
+    },
+    decrement: (state, action) => {
+      const findProduct = state.cartItem.findIndex(item => item.id === action.payload);
+      
+      if (findProduct !== -1 && state.cartItem[findProduct].qty > 1) {
+        state.cartItem[findProduct].qty -= 1;
+        localStorage.setItem('cart', JSON.stringify(state.cartItem));
+      }
+    },
+    removeItem: (state, action) => {
+      state.cartItem = state.cartItem.filter(item => item.id !== action.payload);
+      localStorage.setItem('cart', JSON.stringify(state.cartItem));
+    },
+    clearCart: (state) => {
+      state.cartItem = [];
+      localStorage.setItem('cart', JSON.stringify(state.cartItem));
     }
   }
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, increment, decrement, removeItem, clearCart } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import PageHeading from '../components/PageHeading'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { FaPlus } from "react-icons/fa6";
 import { FaRegWindowMinimize } from "react-icons/fa";
+import { decrement, increment } from '../components/Slice/cartSlice';
 const Cart = () => {
 
  const cartData = useSelector((state)=>state.cartItemManegment.cartItem)
-  
-  
+  const dispatch = useDispatch()
+ const handleIncrement  = (item) => {
+  dispatch(increment(item))
+ }
+  const handleDeIncrement = (item) => {
+    dispatch(decrement(item))
+  }
   return (
     <>
       <PageHeading heading="Shopping Cart"  page="Cart"/>
@@ -16,18 +22,18 @@ const Cart = () => {
       <div className="w-[70%]">
       {cartData.map((item)=>(
        // eslint-disable-next-line react/jsx-key
-       <div className="flex justify-between">
-        <div className="flex items-center ">
+       <div className="flex justify-between border-b-2 pb-4">
+        <div className="flex items-center  gap-5">
         <img className='w-20' src={item.thumbnail} alt="" />
         <h3>{item.title}</h3>
         </div>
         <div className="">
           <span>${item.price}</span>
         </div>
-        <div className="flex items-center gap-4">
-          <FaRegWindowMinimize />
+        <div className="flex items-center bg-[#BEB2C2] gap-4">
+          <FaRegWindowMinimize  onClick={()=>handleIncrement(item)}/>
           <span>{item.qty}</span>
-          <FaPlus />
+          <FaPlus onClick={()=>handleDeIncrement(item)} />
         </div>
         <div className="">
           <span>${(item.price) * (item.qty)}</span>
